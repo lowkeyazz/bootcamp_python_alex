108639f39e434be232b059e18bc9b7b19611c499
thealexjr <80365738+thealexjr@users.noreply.github.com>
2021-04-03 18:52:46 +0200

    Update Ex02.py

--- module00/Ex02.py
+++ module00/Ex02.py
@@ -1,9 +1,9 @@
 def oddchecker(n):
-    if type(n)!=float:
-        print("ERROR")
-    elif n%2==0:
-        print("I'm even.")
-    elif n%2!=0:
-        print("I'm odd.")
-    elif n==0:
+    if n == 0:
         print("I'm zero.")
+    elif n % 2 == 0:
+        print("I'm even.")
+    elif n % 2 != 0:
+        print("I'm odd.")
+    else:
+        print("ERROR")
