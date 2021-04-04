6b3b6f9f8021679ff626068a1eb7b26b103e7a96
thealexjr <80365738+thealexjr@users.noreply.github.com>
2021-04-04 20:35:12 +0200

    Create kata01.py

---
+++ module00/ex05/kata01.py
@@ -0,0 +1,7 @@
+languages = {
+'Python': 'Guido van Rossum',
+'Ruby': 'Yukihiro Matsumoto',
+'PHP': 'Rasmus Lerdorf',
+}
+for language in languages:
+    print(language , "was created by" , languages[language])
