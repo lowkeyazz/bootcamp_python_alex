f97a1bae20f1ec26882e2dd639c54c9eb38aca90
thealexjr <80365738+thealexjr@users.noreply.github.com>
2021-04-05 03:14:11 +0200

    Update recipe.py

--- module00/recipe.py
+++ module00/recipe.py
@@ -1,152 +1,77 @@
+
 cookbook = {
-  "sandwich": {
-    "ingredients": ["ham", "bread", "cheese", "tomatoes"],
-    "meal": "lunch",
-    "prep_time": 10
-  },
-  "cake": {
-    "ingredients": ["flour", "sugar", "eggs"],
-    "meal": "dessert",
-    "prep_time": 60
-  },
-  "salad": {
-    "ingredients": ["avocado", "arugula", "tomatoes", "spinach"],
-    "meal": "lunch",
-    "prep_time": 15
-  }
+    "sandwich": {
+        "ingredients": ["ham", "bread", "cheese", "tomatoes"],
+        "meal": "lunch",
+        "prep_time": 10
+    },
+    "cake": {
+        "ingredients": ["flour", "sugar", "eggs"],
+        "meal": "dessert",
+        "prep_time": 60
+    },
+    "salad": {
+        "ingredients": ["avocado", "arugula", "tomatoes", "spinach"],
+        "meal": "lunch",
+        "prep_time": 15
+    }
 }
 
 
 def print_recipe(recipename):
-    print("recipe for", recipename, ":")
-    print("Ingredients list:", cookbook[recipename]["ingredients"])
-    print("to be eaten for", cookbook[recipename]["meal"], '.')
-    print("takes", cookbook[recipename]["prep_time"], "minutes of cooking.")
+    if recipename in cookbook:
+        print("recipe for", recipename, ":")
+        print("Ingredients list:", cookbook[recipename]["ingredients"])
+        print("to be eaten for", cookbook[recipename]["meal"], '.')
+        print("takes", cookbook[recipename]["prep_time"], "minutes of cooking.")
+    else:
+        print('recipe not found')
 
 
 def delete_recipe(recipename):
     if recipename in cookbook:
         cookbook.pop(recipename, None)
+    else:
+        print('recipe not found')
 
 
 def addrecipe(recipename, ingredientss, meall, prepp_time):
     T = []
     for ingredient in ingredientss:
         T.append(ingredient)
-    new_recipe = {"ingredients": T,
-                "meal": meall,
-                "prep_time": prepp_time, }
+    new_recipe = {"ingredients": T, "meal": meall, "prep_time": prepp_time, }
     cookbook[recipename] = new_recipe
 
 
-addrecipe('tacos', ['poulet', 'frites', 'sauce'], 'lunch', 30)
-
-
 def print_cookbook():
-        for recipename in cookbook:
-                print_recipe(recipename)
+    for recipename in cookbook:
+        print_recipe(recipename)
 
 
 print("Please select an option by typing the corresponding number:")
-print("1: Add a recipe", "2: Delete a recipe ", "3: Print a recipe",
-    "4: Print the cookbook", "5: Quit", sep='\n')
-option = input()
+print("1: Add a recipe", "2: Delete a recipe ", "3: Print a recipe","4: Print the cookbook", "5: Quit", sep='\n')
+option = input('----- ')
 if option == '1':
-    newrecipe = input("enter the new recipe name")
-    print("enter the recipe's ingredients")
+    newrecipe = input("enter the new recipe name: ")
+    print("enter the recipe's ingredients: ")
     a = 'a'
     while a != "":
         T = []
         a = input()
         T.append(a)
-    newmeal = input("enter its meal type")
-    newpreptime = input("enter its preparation time")
+    newmeal = input("enter its meal type: ")
+    newpreptime = input("enter its preparation time: ")
     addrecipe(newrecipe, T, newmeal, newpreptime)
 elif option == '2':
-    recipename = input("enter the name of the recipe you want to delete"))
-    delrecipe(recipename)
+    recipename = input("enter the name of the recipe you want to delete: ")
+    delete_recipe(recipename)
 elif option == '3':
-    recipename=input("enter the name of the recipe you want to print"))
-    print_recipe(recipename)
-elif option == '4':
-    print("your cookbook contains :\n")cookbook={
-  "sandwich": {
-    "ingredients": ["ham", "bread", "cheese", "tomatoes"],
-    "meal": "lunch",
-    "prep_time": 10
-  },
-  "cake": {
-    "ingredients": ["flour", "sugar", "eggs"],
-    "meal": "dessert",
-    "prep_time": 60
-  },
-  "salad": {
-    "ingredients": ["avocado", "arugula", "tomatoes", "spinach"],
-    "meal": "lunch",
-    "prep_time": 15
-  }
-}
-
-
-def print_recipe(recipename):
-    print("recipe for", recipename, ":")
-    print("Ingredients list:", cookbook[recipename]["ingredients"])
-    print("to be eaten for", cookbook[recipename]["meal"], '.')
-    print("takes", cookbook[recipename]["prep_time"], "minutes of cooking.")
-
-
-def delete_recipe(recipename):
-    if recipename in cookbook:
-        cookbook.pop(recipename, None)
-
-
-def addrecipe(recipename, ingredientss, meall, prepp_time):
-    T = []
-    for ingredient in ingredientss:
-        T.append(ingredient)
-    new_recipe = {"ingredients": T,
-                "meal": meall,
-                "prep_time": prepp_time, }
-    cookbook[recipename] = new_recipe
-
-
-addrecipe('tacos', ['poulet', 'frites', 'sauce'], 'lunch', 30)
-
-
-def print_cookbook():
-        for recipename in cookbook:
-                print_recipe(recipename)
-
-
-print("Please select an option by typing the corresponding number:")
-print("1: Add a recipe", "2: Delete a recipe ", "3: Print a recipe",
-    "4: Print the cookbook", "5: Quit", sep='\n')
-option = input()
-if option == '1':
-    newrecipe = input("enter the new recipe name")
-    print("enter the recipe's ingredients")
-    a = 'a'
-    while a != "":
-        T = []
-        a = input()
-        T.append(a)
-    newmeal = input("enter its meal type")
-    newpreptime = input("enter its preparation time")
-    addrecipe(newrecipe, T, newmeal, newpreptime)
-elif option == '2':
-    recipename = input("enter the name of the recipe you want to delete")
-    delrecipe(recipename)
-elif option == '3':
-    recipename=input("enter the name of the recipe you want to print")
+    recipename = input("enter the name of the recipe you want to print: ")
     print_recipe(recipename)
 elif option == '4':
     print("your cookbook contains :\n")
     print_cookbook()
 elif option == '5':
     print("Cookbook closed.")
-else: print("This option does not exist, please type the corresponding number. \n", "To exit, enter 5.")
-
-    print_cookbook()
-elif option == '5':
-    print("Cookbook closed.")
-else: print("This option does not exist, please type the corresponding number. \n", "To exit, enter 5.")
+else:
+    print("This option does not exist, please type the corresponding number. \nTo exit, enter 5.")
