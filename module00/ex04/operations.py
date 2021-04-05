30921848046fee66e06fd7c16c38ba5c4071af3e
thealexjr <80365738+thealexjr@users.noreply.github.com>
2021-04-05 23:43:58 +0200

    Update operations.py

--- module00/ex04/operations.py
+++ module00/ex04/operations.py
@@ -1,15 +1,23 @@
+import sys
+
+
 def operations(a, b):
-    if (not (((type(a) == int) or (type(a) == float)) and ((type(b) == int) or (type(b) == float)))):
-        print("InputError: only numbers \n\nUsage: python operations.py <number1> <number2>\n"
-              "Example:\n    python operations.py 10 3")
+    if not (a.isdigit() and b.isdigit()):
+        print("InputError: only numbers \n\nUsage: python operations.py <number1> <number2>\n","Example:\n    python operations.py 10 3")
         return
-    elif (((type(a) == int) or (type(a) == float)) and ((type(a) == int) or (type(a) == float))):
-        print("Sum:         ", (a + b))
-        print("Difference:  ", (a - b))
-        print("Product:     ", (a * b))
-        if (b == 0):
+    elif (a.isdigit() and b.isdigit()):
+        print("Sum:         ", (float(a) + float(b)))
+        print("Difference:  ", (float(a) - float(b)))
+        print("Product:     ", (float(a) * float(b)))
+        if (float(b) == 0):
             print("Quotient:     ERROR (div by zero)")
             print("Remainder:    ERROR (modulo by zero)")
         else:
-            print("Quotient:    ", (a / b))
-            print("Remainder:   ", (a % b))
+            print("Quotient:    ", (float(a) / float(b)))
+            print("Remainder:   ", (float(a) % float(b)))
+
+
+if len(sys.argv) == 3:
+    operations(sys.argv[1], sys.argv[2])
+else:
+    print("InputError: only numbers \n\nUsage: python operations.py <number1> <number2>\n","Example:\n    python operations.py 10 3")
