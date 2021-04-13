295d943be14c4d065e22d2d430f88dfbf76fd43d
thealexjr <80365738+thealexjr@users.noreply.github.com>
2021-04-14 01:46:38 +0200

    Create test.py

---
+++ module01/ex02/test.py
@@ -0,0 +1,17 @@
+from vector import Vector
+
+v1=Vector([1.0,2.0,3.0,4.0])
+print(v1)
+v2 = v1 * 5
+print(v2)
+print(v1+v2)
+print(v2+6)
+v3=Vector(4)
+print(v3)
+v4=Vector((1,5))
+print(v4)
+print(v3-v1)
+print(v4-5)
+print(v1*v4)
+print(v4/2)
+print("\n everything is working")
